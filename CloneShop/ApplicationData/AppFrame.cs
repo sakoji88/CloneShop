a9f0ef89ea948bbd49a3517fac41ca5b74b79e1d
sakoji88 <slonionok0407@gmail.com>
2026-04-07 01:32:05 +0300

    Добавьте файлы проекта.

---
+++ CloneShop/ApplicationData/AppFrame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CloneShop.ApplicationData
+{
+    internal class AppFrame
+    {
+        public static Frame frmMain;
+    }
+}
---
+++ CloneShop/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using CloneShop.ApplicationData;
+using CloneShop.Pages;
+
+namespace CloneShop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitializeDatabaseConnection();
+            SetupNavigationSystem();
+            LoadAuthorizationPage();
+        }
+
+        private void InitializeDatabaseConnection()
+        {
+            try
+            {
+                AppConnect.model01 = new CloneShopDBEntities();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(
+                    "Не удалось подключиться к базе данных:\n" + ex.Message,
+                    "Ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private void SetupNavigationSystem()
+        {
+            AppFrame.frmMain = FrmMain;
+        }
+
+        private void LoadAuthorizationPage()
+        {
+            FrmMain.Navigate(new AutorizationPage());
+        }
+    }
+}
