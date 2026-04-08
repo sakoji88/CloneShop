ac5ac18792f5fdac8a72e36121fcc888453fd5a4
10230037 <10230037@D112PC19>
2026-04-08 15:42:34 +0300

    ffe

--- CloneShop/Pages/AutorizationPage.xaml.cs
+++ CloneShop/Pages/AutorizationPage.xaml.cs
@@ -25,11 +25,42 @@ namespace CloneShop.Pages
         public AutorizationPage()
         {
             InitializeComponent();
+
         }
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            
+            string login = txtLogin.Text;
+            string password = txtPassword.Password;
+
+            if (string.IsNullOrWhiteSpace(login))
+            {
+                MessageBox.Show("Введите логин");
+                txtLogin.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Введите пароль");
+                txtPassword.Focus();
+                return;
+            }
+
+            var userObj = AppConnect.model01.Users.FirstOrDefault(x =>
+                x.Login == login && x.Password == password);
+
+            if (userObj == null)
+            {
+                MessageBox.Show("Неверный логин или пароль");
+                txtPassword.Clear();
+                txtLogin.Focus();
+                return;
+            }
+
+            MessageBox.Show("Добро пожаловать, " + userObj.FullName);
+            AppConnect.CurrentUser = userObj;
+            AppFrame.frmMain.Navigate(new CatalogPage());
         }
 
         private void btnReg_Click(object sender, RoutedEventArgs e)
--- CloneShop/Pages/CatalogPage.xaml.cs
+++ CloneShop/Pages/CatalogPage.xaml.cs
@@ -38,7 +38,10 @@ namespace CloneShop.Pages
                 tbRole.Text = "Роль: Пользователь";
                 btnAdmin.Visibility = System.Windows.Visibility.Collapsed;
             }
-            lvProducts.ItemsSource = AppConnect.model01.Products.ToList();
+            
+            LoadCategories();
+            LoadSort();
+            LoadProducts();
         }
 
         private void btnExit_Click(object sender, RoutedEventArgs e)
@@ -52,5 +55,78 @@ namespace CloneShop.Pages
         {
             MessageBox.Show("Го в админку");
         }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            var products = AppConnect.model01.Products.ToList();
+
+            if (cmbCategory.SelectedIndex > 0)
+            {
+                string selectedCategory = cmbCategory.SelectedItem.ToString();
+
+                products = products.Where(x =>
+                    x.Categories.CategoryName == selectedCategory
+                ).ToList();
+            }
+
+            if (!string.IsNullOrWhiteSpace(txtSearch.Text))
+            {
+                products = products.Where(x =>
+                    x.ProductName.ToLower().Contains(txtSearch.Text.ToLower()) ||
+                    (x.Description != null && x.Description.ToLower().Contains(txtSearch.Text.ToLower()))
+                ).ToList();
+            }
+            if (cmbSort.SelectedIndex == 1)
+            {
+                products = products.OrderBy(x => x.Price).ToList();
+            }
+            else if (cmbSort.SelectedIndex == 2)
+            {
+                products = products.OrderByDescending(x => x.Price).ToList();
+            }
+
+            lvProducts.ItemsSource = products;
+        }
+
+
+        private void cmbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadProducts();
+        }
+        private void LoadCategories()
+        {
+            cmbCategory.Items.Clear();
+            cmbCategory.Items.Add("Все категории");
+
+            var categories = AppConnect.model01.Categories.ToList();
+
+            foreach (var category in categories)
+            {
+                cmbCategory.Items.Add(category.CategoryName);
+            }
+
+            cmbCategory.SelectedIndex = 0;
+        }
+        private void LoadSort()
+        {
+            cmbSort.Items.Clear();
+
+            cmbSort.Items.Add("Без сортировки");
+            cmbSort.Items.Add("Цена по возрастанию");
+            cmbSort.Items.Add("Цена по убыванию");
+
+            cmbSort.SelectedIndex = 0;
+        }
+
+        private void cmbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadProducts();
+        }
     }
+
 }
